c5a11dc1af6d6918867e45024bf64191aa46fe2b
Neto Nobre <igornobre2016@hotmail.com>
2024-11-02 17:57:34 -0300

    frig

--- noivadomaijr/src/App.js
+++ noivadomaijr/src/App.js
@@ -18,7 +18,7 @@ const CatalogoPresentes = () => {
   };
 
   // Lista de presentes escolhidos
-  const presentesEscolhidos = ['Televisão', 'MOP', 'Ventilador', 'Aspirador de pó', 'Panela de pressão', 'Tapete para banheiro', 'Cortina Blackout', 'Lixeira para cozinha', 'Sanduicheira elétrica', 'Conjunto de copos', 'Kit Utensilios em silicone', 'Kit Utensilios em inox', 'Cuscuzeiro', 'Aparelho de jantar'];
+  const presentesEscolhidos = ['Televisão', 'MOP', 'Fruteira', 'Frigideiras antiaderente média', 'Ventilador', 'Aspirador de pó', 'Panela de pressão', 'Tapete para banheiro', 'Cortina Blackout', 'Lixeira para cozinha', 'Sanduicheira elétrica', 'Conjunto de copos', 'Kit Utensilios em silicone', 'Kit Utensilios em inox', 'Cuscuzeiro', 'Aparelho de jantar'];
 
   // Função que verifica se um presente já foi escolhido
   const isEscolhido = (presente) => presentesEscolhidos.includes(presente);
